974d5fa245cc908f1115e7b5133bb513e33bcd8b
web-GIS-RES <ht101f@gmail.com>
2025-09-30 21:32:55 +0300

    Fix map rendering (TileLayer + solid container height) and move NEW INSTALLATION button left

--- src/map/MapView.tsx
+++ src/map/MapView.tsx
@@ -1,4 +1,4 @@
-import { MapContainer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import InstallationsLayer from "./InstallationsLayer";
 import { useState } from "react";
@@ -6,11 +6,17 @@ import NewInstallation from "../ui/NewInstallation";
 
 export default function MapView() {
   const [refreshKey, setRefreshKey] = useState(0);
-
   const bump = () => setRefreshKey((k) => k + 1);
 
   return (
-    <div style={{ height: "100vh", width: "100%", position: "relative" }}>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100vh", // 100% του viewport ύψους
+        overflow: "hidden",
+      }}
+    >
       <MapContainer
         center={[40.300982, 21.789813]}
         zoom={11}
@@ -19,16 +25,23 @@ export default function MapView() {
         zoomControl={true}
         style={{ width: "100%", height: "100%" }}
       >
+        {/* Βασικά πλακίδια για να φαίνεται ο χάρτης */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
         <InstallationsLayer refreshKey={refreshKey} />
       </MapContainer>
 
-      {/* Τοποθέτηση του κουμπιού NEW INSTALLATION λίγο πιο αριστερά */}
+      {/* Μετακινημένο κουμπί NEW INSTALLATION ώστε να μην ακουμπά τα zoom controls */}
       <div
         style={{
           position: "absolute",
-          top: "10px",
-          right: "60px", // αντί για 10px ώστε να μην ακουμπά τα zoom controls
+          top: 10,
+          right: 60, // λίγο αριστερότερα από τα zoom controls
           zIndex: 1000,
+          pointerEvents: "auto",
         }}
       >
         <NewInstallation onCreated={bump} />
