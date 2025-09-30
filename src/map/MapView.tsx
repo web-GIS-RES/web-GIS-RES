edf7080f85ed8a68460d7e6717de3f645c38f32b
web-GIS-RES <ht101f@gmail.com>
2025-09-30 21:24:00 +0300

    Move NEW INSTALLATION button slightly left to avoid overlapping zoom controls

--- src/map/MapView.tsx
+++ src/map/MapView.tsx
@@ -1,50 +1,38 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, ZoomControl, ScaleControl, useMap } from "react-leaflet";
+import { MapContainer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import InstallationsLayer from "./InstallationsLayer";
+import { useState } from "react";
 import NewInstallation from "../ui/NewInstallation";
-import RegionFilter from "../ui/RegionFilter"; // αν δεν υπάρχει, αφαίρεσέ το import
-
-function SetGlobalMap() {
-  const map = useMap();
-  useEffect(() => {
-    (window as any).__leafletMap = map;
-  }, [map]);
-  return null;
-}
 
 export default function MapView() {
   const [refreshKey, setRefreshKey] = useState(0);
+
   const bump = () => setRefreshKey((k) => k + 1);
 
-  const center: [number, number] = [39.2, 22.0];
-
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      {typeof RegionFilter === "function" ? <RegionFilter /> : null}
-
+    <div style={{ height: "100vh", width: "100%", position: "relative" }}>
       <MapContainer
-        center={center}
-        zoom={7}
-        minZoom={5}
+        center={[40.300982, 21.789813]}
+        zoom={11}
+        minZoom={9}
         maxZoom={18}
-        zoomControl={false}
+        zoomControl={true}
         style={{ width: "100%", height: "100%" }}
       >
-        <SetGlobalMap />
-
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <ScaleControl position="bottomleft" />
-        <ZoomControl position="topright" />
-
         <InstallationsLayer refreshKey={refreshKey} />
       </MapContainer>
 
-      <NewInstallation onCreated={bump} />
+      {/* Τοποθέτηση του κουμπιού NEW INSTALLATION λίγο πιο αριστερά */}
+      <div
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "60px", // αντί για 10px ώστε να μην ακουμπά τα zoom controls
+          zIndex: 1000,
+        }}
+      >
+        <NewInstallation onCreated={bump} />
+      </div>
     </div>
   );
 }
